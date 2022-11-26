4461b747181a54e50ad2b7ac4abfce0cf73ea2c9
Adrian Nieves de la Cruz <ingadriannievescruz@gmail.com>
2022-11-26 22:37:09 +0100

    [ADD] Bat file  electron

--- physics.bat
+++ physics.bat
@@ -1,5 +1,13 @@
 call ionic build
-call npx cap copy
-call npx cap sync
-npx cap open android
- 
+
+@REM  Android app
+
+@REM call npx cap copy
+@REM call npx cap sync
+@REM npx cap open android
+
+@REM  Desktop app
+
+call npx cap copy @capacitor-community/electron
+call npx cap sync @capacitor-community/electron
+call ionic cap open electron
