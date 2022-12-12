ce1d58bce5ea538629a22b521a630d30ddb9f2bb
Jose Antonio Alvarez Espinosa <tonialv94@gmail.com>
2022-12-12 02:12:31 +0100

    progress

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -8,8 +8,10 @@ export class ApiService {
   url_server: string = 'https://moviesapi20221211011349.azurewebsites.net';
 
   user: any;
-
+ 
   constructor(private http: HttpClient) {}
+
+  getUser(){return this.user;};
 
   async login(email: string, password: string): Promise<boolean> {
     return new Promise<boolean>(async (resolve, reject) => {
@@ -41,22 +43,24 @@ export class ApiService {
   }
 
   async saveExamResults(body: any): Promise<any> {
+    console.log("body",body);
     return new Promise<boolean>(async (resolve, reject) => {
       let postParams = {
         id_student: this.user.id,
       };
 
       let final = { ...postParams, ...body };
-
+      console.log("final",final);
       try {
         this.http
           .post(`${this.url_server}/exam`, final)
           .subscribe(async (data: any) => {
-            if (data) {
+            resolve(true);
+          /*  if (data) {
               resolve(true);
             } else {
               resolve(false);
-            }
+            }*/
           });
       } catch (err) {
         console.log('************* err *************');
@@ -65,17 +69,18 @@ export class ApiService {
     });
   }
 
-  //   async saveExamResults(id: number): Promise<any> {
-  //     return new Promise<boolean>(async (resolve, reject) => {
-  //       this.http
-  //         .get(`${this.url_server}/user/${id}/footprints`)
-  //         .subscribe(async (data: any) => {
-  //           if (data) {
-  //             resolve(data);
-  //           } else {
-  //             reject();
-  //           }
-  //         });
-  //     });
-  //   }
+   async getExamResults(): Promise<any> {
+    return new Promise<any>(async (resolve, reject) => {
+      this.http
+           .get(`${this.url_server}/exam/${this.user.id}`)
+           .subscribe(async (data: any) => {
+            if (data) {
+              
+              resolve(data);
+           } else {
+               reject();
+            }
+           });
+       });
+    }
 }
