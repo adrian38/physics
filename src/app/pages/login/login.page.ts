25348399d811b8ef0e44a21cb027d805696e84a2
Adrian Nieves de la Cruz <ingadriannievescruz@gmail.com>
2024-03-28 20:14:36 +0100

    many changes

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,165 +1,167 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AlertController,
-  LoadingController,
-  NavController,
-  Platform,
-} from '@ionic/angular';
+import { AlertController, LoadingController, NavController, Platform } from '@ionic/angular';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 // import { HttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/services/backend.service';
+import { UserService } from 'src/app/services/user.service';
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  loading: HTMLIonLoadingElement = null;
-  btn_disabled: boolean = false;
-  user = new UsuarioModel();
+    loading: HTMLIonLoadingElement = null;
+    btn_disabled: boolean = false;
+    user = new UsuarioModel();
 
-  constructor(
-    public navController: NavController,
-    public alertController: AlertController,
-    public platform: Platform,
-    public loadingController: LoadingController,
-    // private https: HttpClient,
-    private _apiService: ApiService
-  ) {}
+    constructor(
+        public navController: NavController,
+        public alertController: AlertController,
+        public platform: Platform,
+        public loadingController: LoadingController,
+        // private https: HttpClient,
+        private _apiService: ApiService,
+        public _user: UserService
+    ) {}
 
-  ngOnInit() {
-    this.subscriptions();
-  }
-  ngOnDestroy() {}
+    ngOnInit() {
+        this.subscriptions();
+    }
+    ngOnDestroy() {}
 
-  async login(email: string, password: string) {
-    this.showLoading('Verificando');
+    async login(email: string, password: string) {
+        this.showLoading('Verificando');
+        localStorage.setItem('access_token', '');
+        sessionStorage.setItem('user', '');
 
-    try {
-      if (await this._apiService.login(email, password)) {
-        this.checkUser();
-      } else {
+        try {
+            if (await this._user.login(this.user.email, password)) {
+                // this.checkUser();
+            } else {
+                this.handleLoginError();
+            }
+        } catch (err) {
+            this.handleLoginError();
+        }
+        this.loading.dismiss();
+    }
+
+    private handleLoginError() {
         this.btn_disabled = false;
         this.showAlert();
-      }
-    } catch (err) {
-      this.btn_disabled = false;
-      this.showAlert();
     }
-    this.loading.dismiss();
-  }
 
-  subscriptions() {
-    this.platform.backButton.subscribeWithPriority(10, () => {
-      this.showExitAlert();
-    });
-  }
-
-  showExitAlert() {
-    this.alertController
-      .create({
-        header: 'Alerta',
-        message: 'Desea salir de la aplicación?',
-        backdropDismiss: false,
-        buttons: [
-          {
-            text: 'Cancelar',
-            role: 'cancel',
-            handler: () => {},
-          },
-          {
-            text: 'Salir',
-            handler: () => {
-              navigator['app'].exitApp();
-            },
-          },
-        ],
-      })
-      .then((alert) => {
-        alert.present();
-      });
-  }
-
-  checkUser() {
-    this.user = this._apiService.getUser();
-    console.log(this.user, "user");
-    if (this.user.type === 'Estudiante') {
-      if (this.loading) {
-        this.loading.dismiss();
-      }
-      this.navController.navigateRoot('/home', {
-        animated: true,
-        animationDirection: 'forward',
-      });
-    } else {
-      if (this.loading) {
-        this.loading.dismiss();
-      }
-      this.btn_disabled = false;
-      this.showAlertProfesor();
+    subscriptions() {
+        this.platform.backButton.subscribeWithPriority(10, () => {
+            this.showExitAlert();
+        });
     }
-  }
 
-  onSubmit() {
-    // console.log(this.user, 'user');
-    this.login(this.user.email, this.user.password);
-  }
+    showExitAlert() {
+        this.alertController
+            .create({
+                header: 'Alerta',
+                message: 'Desea salir de la aplicación?',
+                backdropDismiss: false,
+                buttons: [
+                    {
+                        text: 'Cancelar',
+                        role: 'cancel',
+                        handler: () => {},
+                    },
+                    {
+                        text: 'Salir',
+                        handler: () => {
+                            navigator['app'].exitApp();
+                        },
+                    },
+                ],
+            })
+            .then((alert) => {
+                alert.present();
+            });
+    }
 
-  async showAlert() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Usuario deshabilitado o mal escrito',
-      message: 'Si el problema persiste contactar con la administración',
-      buttons: [
-        {
-          text: 'Cerrar',
-          role: 'cancel',
-          cssClass: 'secondary',
-        },
-      ],
-    });
+    // checkUser() {
+    //     this.user = this._apiService.getUser();
+    //     console.log(this.user, 'user');
+    //     if (this.user.type === 'Estudiante') {
+    //         if (this.loading) {
+    //             this.loading.dismiss();
+    //         }
+    //         this.navController.navigateRoot('/home', {
+    //             animated: true,
+    //             animationDirection: 'forward',
+    //         });
+    //     } else {
+    //         if (this.loading) {
+    //             this.loading.dismiss();
+    //         }
+    //         this.btn_disabled = false;
+    //         this.showAlertProfesor();
+    //     }
+    // }
 
-    await alert.present();
-  }
+    onSubmit() {
+        // console.log(this.user, 'user');
+        this.login(this.user.email, this.user.password);
+    }
 
-  async showAlertProfesor() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Por favor utilice la aplicacion de profesor',
-      message: 'Si el problema persiste contactar con la administración',
-      buttons: [
-        {
-          text: 'Cerrar',
-          role: 'cancel',
-          cssClass: 'secondary',
-        },
-      ],
-    });
+    async showAlert() {
+        const alert = await this.alertController.create({
+            cssClass: 'my-custom-class',
+            header: 'Usuario deshabilitado o mal escrito',
+            message: 'Si el problema persiste contactar con la administración',
+            buttons: [
+                {
+                    text: 'Cerrar',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                },
+            ],
+        });
 
-    await alert.present();
-  }
+        await alert.present();
+    }
 
-  async showLoading(message: string) {
-    this.btn_disabled = true;
-    this.loading = await this.loadingController.create({
-      cssClass: 'my-custom-class',
-      message: message,
-    });
+    async showAlertProfesor() {
+        const alert = await this.alertController.create({
+            cssClass: 'my-custom-class',
+            header: 'Por favor utilice la aplicacion de profesor',
+            message: 'Si el problema persiste contactar con la administración',
+            buttons: [
+                {
+                    text: 'Cerrar',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                },
+            ],
+        });
 
-    return this.loading.present();
-  }
+        await alert.present();
+    }
 
-  forgottenPassword() {
-    // this.navController.navigateRoot('/user-password-lost', {
-    //   animated: true,
-    //   animationDirection: 'forward',
-    // });
-  }
+    async showLoading(message: string) {
+        this.btn_disabled = true;
+        this.loading = await this.loadingController.create({
+            cssClass: 'my-custom-class',
+            message: message,
+        });
 
-  register_user(){
-    this.navController.navigateRoot('/register', {
-        animated: true,
-       animationDirection: 'forward',
-     });
-  }
+        return this.loading.present();
+    }
+
+    forgottenPassword() {
+        // this.navController.navigateRoot('/user-password-lost', {
+        //   animated: true,
+        //   animationDirection: 'forward',
+        // });
+    }
+
+    register_user() {
+        this.navController.navigateRoot('/register', {
+            animated: true,
+            animationDirection: 'forward',
+        });
+    }
 }
